43176b2e2743363d469bcb9b1c5dc63065f9a91e
C S Job <csjob78@gmail.com>
2021-08-07 19:20:10 +0530

    ASP.NET Web API and SQL Server➡ All the 4 http methods connected with database table.✔

---
+++ WebApiEmployee/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiEmployee
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            //for json indent
+           // config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+        
+        }
+    }
+}
